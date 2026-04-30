17efcd7140e6ccda6ba985c453e17c909ef8a1b0
Shivmalhar Gulave <159407624+shivgithub2005@users.noreply.github.com>
2026-04-30 14:10:09 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,37 +1,20 @@
+from pipelines.pipeline import run_pipeline
+from utils.logger import setup_logger
 import logging
-from sqlalchemy import engine
-from extract import extract_data
-from transform import transform_data
 
-logging.basicConfig(
-    filename='logs/project.log',
-    level=logging.INFO,
-    format='%(asctime)s - %(levelname)s - %(message)s'
-)
 
 def main():
-    logging.info("Project Started")
+    # Setup logging
+    setup_logger()
+    logging.info("Starting Vendor Performance ETL Pipeline")
 
-    df,engine = extract_data()
-    logging.info("Data Extracted")
+    try:
+        run_pipeline()
+        logging.info("Pipeline executed successfully")
+    except Exception as e:
+        logging.error(f"Pipeline failed: {e}")
+        raise
 
-    df = transform_data(df)
-    logging.info("Data Transformed")
-
-    print(df.info())
-
-    # Save for Power BI
-    df.to_csv("data/final_data.csv", index=False)
-    logging.info("Data Saved")
-
-    df.to_sql(
-        name = "final_data",
-        con = engine,
-        if_exists = "replace",
-        index = False
-    )
-
-    logging.info("Project Completed")
 
 if __name__ == "__main__":
     main()
